dfc9dd7ed8ca5e9242448528e51802115fc3981e
sorayahaidars <sorayahaidars14@gmail.com>
2024-03-13 05:28:24 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul4_1302223006/Program.cs
+++ tpmodul4_1302223006/Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class KodePos
+{
+    public string GetKodePos(string kelurahan)
+    {
+        string[] kelurahanArray = new string[]
+        {
+            "Batununggal",
+            "Kujangsari",
+            "Mengger",
+            "Wates",
+            "Cijaura",
+            "Jatisari",
+            "Margasari",
+            "Sekejati",
+            "Kebonwaru",
+            "Maleer",
+            "Samoja"
+        };
+
+        string[] kodePosArray = new string[]
+        {
+            "40266",
+            "40287",
+            "40267",
+            "40256",
+            "40287",
+            "40286",
+            "40286",
+            "40286",
+            "40272",
+            "40274",
+            "40273"
+        };
+
+        for (int i = 0; i < kelurahanArray.Length; i++)
+        {
+            if (kelurahan.Equals(kelurahanArray[i]))
+            {
+                return kodePosArray[i];
+            }
+        }
+
+        return "Kelurahan tidak ditemukan";
+    }
+}
+
+public enum DoorState
+{
+    Locked,
+    Open
+}
+
+public class DoorMachine
+{
+    private DoorState state = DoorState.Locked;
+
+    public void LockDoor()
+    {
+        state = DoorState.Locked;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void OpenDoor()
+    {
+        if (state == DoorState.Locked)
+        {
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu terkunci");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+
+        Console.WriteLine("Masukkan nama kelurahan: ");
+        string kelurahan = Console.ReadLine();
+
+        string kodePosnya = kodePos.GetKodePos(kelurahan);
+
+        Console.WriteLine($"Kode pos untuk kelurahan {kelurahan} adalah {kodePosnya}");
+
+        DoorMachine doorMachine = new DoorMachine();
+
+        doorMachine.LockDoor();  
+        doorMachine.OpenDoor();
+    }
+}
+
+
--- tpmodul4_1302223006/obj/Debug/net8.0/tpmodul4_1302223006.AssemblyInfo.cs
+++ tpmodul4_1302223006/obj/Debug/net8.0/tpmodul4_1302223006.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_1302223006")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9892cafd0999d324e361da3a5a202369e7d93b9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_1302223006")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_1302223006")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
